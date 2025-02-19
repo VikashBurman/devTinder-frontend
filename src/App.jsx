7146957c19a2aadc98b51bbdb364379ee326ce06
Vikash Burman <vikasburman37@gmail.com>
2025-02-19 14:51:48 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import NavBar from "./NavBar";
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <h1 className="text-4xl font-bold underline">Hello world!</h1>
+    </>
+  );
+}
+
+export default App;
